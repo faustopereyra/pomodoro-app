c38eed1d51c2860e79266a0e878952df0c9cffd3
faustopereyra <pfausto@live.com.ar>
2020-07-19 02:48:57 -0300

    Timer Reducer

--- src/components/timer/timer.component.jsx
+++ src/components/timer/timer.component.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
+import { connect } from "react-redux"
+
 import "./timer.style.scss"
 
-const Timer = () => {
+const Timer = ({ actualTime }) => {
+
+    const seconds = actualTime / 1000;
+    const minutes = seconds / 60;
+
+    const secondsDisplay = seconds % 60
+    const minutesDisplay = minutes % 60
 
     return (
         <div className="timer-group">
 
             <div className="timer minute">
-                <div className="hand"><span style={{ "animation-duration": "10s" }} ></span></div>
-                <div className="hand"><span style={{ "animation-duration": "10s" }}></span></div>
+                <div className="hand"><span style={{ "animation-duration": `${seconds}s` }} ></span></div>
+                <div className="hand"><span style={{ "animation-duration": `${seconds}s` }}></span></div>
             </div>
             <div className="timer second">
                 <div className="hand"><span></span></div>
@@ -17,11 +25,16 @@ const Timer = () => {
             </div>
             <div className="face">
                 <h2>Motivational quote</h2>
-                <p id="lazy">00:00:00</p>
+                <p id="lazy">{`${minutesDisplay} : ${secondsDisplay} `}</p>
             </div>
         </div >
     );
 
 
 };
-export default Timer;
+
+const mapStateToProps = state => ({
+    actualTime: state.timer.actualTime
+});
+
+export default connect(mapStateToProps)(Timer);
--- src/redux/timer/timer.reducer.js
+++ src/redux/timer/timer.reducer.js
@@ -1,6 +1,16 @@
-const INITTIAL_STATE = {
+const INITIAL_STATE = {
     timerStage: "work",
     actualTime: 1500000,
     defaultWorkTime: 1500000,
     defaultRestTime: 300000
 }
+
+const timerReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+}
+
+export default timerReducer;
